fe799190365091eb0d0c34f11f824e3ed02d7ef8
ricardo_hvarela <rvarela.contractor@luminatedata.com>
2023-10-09 17:25:33 -0600

    Add configuration for rest assured

--- src/test/java/TestOne.java
+++ src/test/java/TestOne.java
@@ -1,20 +1,39 @@
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TestOne {
 
+    @BeforeEach
+    public void setup() {
+        RestAssured.baseURI = "https://reqres.in";
+        RestAssured.basePath = "/api";
+    }
+
     @Test
     public void testLoginSuccess(){
 
-        String response = RestAssured.given()
+        RestAssured.given()
             .contentType(ContentType.JSON)
             .body("{\n"
                 + "    \"email\": \"eve.holt@reqres.in\",\n"
                 + "    \"password\": \"cityslicka\"\n"
                 + "}")
-            .post("https://reqres.in/api/login")
-            .then().log().all().extract().asString();
+            .post("/login")
+            .then()
+            .statusCode(HttpStatus.SC_OK)
+            .log().all().extract().asString();
 
+    }
+
+    @Test
+    public void testGetUsers() {
+        RestAssured.given().when().get("/users/2")
+            .then()
+            .statusCode(HttpStatus.SC_OK)
+            .body("data.id", Matchers.equalTo(2));
     }
 }
