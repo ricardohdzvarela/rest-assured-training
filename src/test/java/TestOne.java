952c7dc7a6bbc5d01ed4110020df5cb191a3ef33
ricardo_hvarela <rvarela.contractor@luminatedata.com>
2023-10-11 13:38:28 -0600

    add json schemas and validate

--- src/test/java/TestOne.java
+++ src/test/java/TestOne.java
@@ -2,6 +2,8 @@ import io.restassured.RestAssured;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import java.io.InputStream;
 import org.apache.http.HttpStatus;
 import org.example.helpers.JsonDataProviderReader;
 import org.hamcrest.Matchers;
@@ -13,6 +15,7 @@ public class TestOne {
 
     JSONObject loginData;
     JSONObject userData;
+    JSONObject resourceData;
 
     @BeforeEach
     public void setup() {
@@ -20,12 +23,13 @@ public class TestOne {
         RestAssured.basePath = "/api";
         RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
         JsonDataProviderReader jsonDataProviderReader = new JsonDataProviderReader();
-        loginData = jsonDataProviderReader.readJsonData("src/main/resources/userLoginData.json");
-        userData = jsonDataProviderReader.readJsonData("src/main/resources/userData.json");
+        loginData = jsonDataProviderReader.readJsonData("src/main/resources/user/userLoginData.json");
+        userData = jsonDataProviderReader.readJsonData("src/main/resources/user/userData.json");
+        resourceData = jsonDataProviderReader.readJsonData("src/main/resources/resource/resourceData.json");
     }
 
     @Test
-    public void testLoginSuccess(){
+    public void testLoginSuccessful(){
         JSONObject successLoginData = (JSONObject) loginData.get("success_login_data");
         RestAssured.given()
             .contentType(ContentType.JSON)
@@ -34,52 +38,96 @@ public class TestOne {
             .then()
             .statusCode(HttpStatus.SC_OK)
             .extract().asString();
-
     }
 
     @Test
-    public void testLoginBadRequest(){
+    public void testLoginBadRequest() {
         JSONObject badRequestLoginData = (JSONObject) loginData.get("bad_request_login_data");
         RestAssured.given()
             .contentType(ContentType.JSON)
             .body(badRequestLoginData.toJSONString())
             .post("/login")
             .then()
-            .statusCode(HttpStatus.SC_OK)
+            .statusCode(HttpStatus.SC_BAD_REQUEST)
             .extract().asString();
-
     }
 
     @Test
-    public void testGetUser() {
+    public void testGetListUsers() {
+        InputStream getUserListSchemaResponse = getClass().getClassLoader()
+            .getResourceAsStream("responseSchemas/getUserListSchemaResponse.json");
+
+        RestAssured.given()
+            .when()
+            .get("/users")
+            .then()
+            .statusCode(HttpStatus.SC_OK)
+            .and()
+            .assertThat()
+            .body(JsonSchemaValidator.matchesJsonSchema(getUserListSchemaResponse));
+    }
+
+    @Test
+    public void testGetSingleUser() {
+        InputStream getUserSchemaResponse = getClass().getClassLoader()
+            .getResourceAsStream("responseSchemas/getUserSchemaResponse.json");
+
         RestAssured.given()
             .when()
             .get("/users/2")
             .then()
             .statusCode(HttpStatus.SC_OK)
-            .body("data.id", Matchers.equalTo(2));
+            .and()
+            .assertThat()
+            .body(JsonSchemaValidator.matchesJsonSchema(getUserSchemaResponse));
     }
 
     @Test
-    public void testDeleteUser() {
+    public void testGetSingleUserNotFound() {
         RestAssured.given()
             .when()
-            .delete("/users/2")
+            .get("/users/23")
             .then()
-            .statusCode(HttpStatus.SC_NO_CONTENT);
+            .statusCode(HttpStatus.SC_NOT_FOUND);
     }
 
     @Test
-    public void testPatchUser() {
-        JSONObject updateUserData = (JSONObject) userData.get("update_user");
+    public void testGetListResource() {
+        InputStream getResourceListSchema = getClass().getClassLoader()
+            .getResourceAsStream("responseSchemas/getResourceListSchemaResponse.json");
+
         RestAssured.given()
-            .contentType(ContentType.JSON)
-            .body(updateUserData.toJSONString())
-            .patch("/users/2")
+            .when()
+            .get("/unknown")
             .then()
             .statusCode(HttpStatus.SC_OK)
-            .body("name", Matchers.equalTo("morpheus"))
-            .body("job", Matchers.equalTo("zion resident"));
+            .and()
+            .assertThat()
+            .body(JsonSchemaValidator.matchesJsonSchema(getResourceListSchema));
+    }
+
+    @Test
+    public void testGetSingleResource() {
+        InputStream getResourceSchema = getClass().getClassLoader()
+            .getResourceAsStream("responseSchemas/getResourceSchemaResponse.json");
+
+        RestAssured.given()
+            .when()
+            .get("/unknown/2")
+            .then()
+            .statusCode(HttpStatus.SC_OK)
+            .and()
+            .assertThat()
+            .body(JsonSchemaValidator.matchesJsonSchema(getResourceSchema));
+    }
+
+    @Test
+    public void testGetSingleResourceNotFound() {
+        RestAssured.given()
+            .when()
+            .get("/unknown/23")
+            .then()
+            .statusCode(HttpStatus.SC_NOT_FOUND);
     }
 
     @Test
@@ -93,5 +141,77 @@ public class TestOne {
             .statusCode(HttpStatus.SC_CREATED)
             .body("name", Matchers.equalTo("morpheus"))
             .body("job", Matchers.equalTo("leader"));
+    }
+
+    @Test
+    public void testPatchUser() {
+        InputStream updateUserSchema = getClass().getClassLoader()
+            .getResourceAsStream("responseSchemas/updateUserSchemaResponse.json");
+        JSONObject updateUserData = (JSONObject) userData.get("update_user");
+
+        RestAssured.given()
+            .contentType(ContentType.JSON)
+            .body(updateUserData.toJSONString())
+            .patch("/users/2")
+            .then()
+            .statusCode(HttpStatus.SC_OK)
+            .and()
+            .assertThat()
+            .body(JsonSchemaValidator.matchesJsonSchema(updateUserSchema));
+    }
+
+    @Test
+    public void testUpdateUser() {
+        InputStream updateUserSchema = getClass().getClassLoader()
+            .getResourceAsStream("responseSchemas/updateUserSchemaResponse.json");
+        JSONObject updateUserData = (JSONObject) userData.get("update_user");
+
+        RestAssured.given()
+            .contentType(ContentType.JSON)
+            .body(updateUserData.toJSONString())
+            .put("/users/2")
+            .then()
+            .statusCode(HttpStatus.SC_OK)
+            .and()
+            .assertThat()
+            .body(JsonSchemaValidator.matchesJsonSchema(updateUserSchema));
+    }
+
+    @Test
+    public void testDeleteUser() {
+        RestAssured.given()
+            .when()
+            .delete("/users/2")
+            .then()
+            .statusCode(HttpStatus.SC_NO_CONTENT);
+    }
+
+    @Test
+    public void testPostRegisterSuccessful() {
+        InputStream postRegisterSchema = getClass().getClassLoader()
+            .getResourceAsStream("responseSchemas/postRegisterSchemaResponse.json");
+        JSONObject createResource = (JSONObject) resourceData.get("create_resource");
+
+        RestAssured.given()
+            .contentType(ContentType.JSON)
+            .body(createResource.toJSONString())
+            .post("/register")
+            .then()
+            .statusCode(HttpStatus.SC_OK)
+            .and()
+            .assertThat()
+            .body(JsonSchemaValidator.matchesJsonSchema(postRegisterSchema));
+    }
+
+    @Test
+    public void testPostRegisterUnsuccessful() {
+        JSONObject resourceBadRequest = (JSONObject) resourceData.get("resource_bad_request");
+
+        RestAssured.given()
+            .contentType(ContentType.JSON)
+            .body(resourceBadRequest.toJSONString())
+            .post("/register")
+            .then()
+            .statusCode(HttpStatus.SC_BAD_REQUEST);
     }
 }
